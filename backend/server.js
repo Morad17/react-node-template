2856faf96203c300a914e3c7c7de1aa2b29582e9
morad17 <morad123@hotmail.co.uk>
2025-08-19 21:30:33 +0100

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = process.env.PORT || 8000;
+
+// Initialise Node App//
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
